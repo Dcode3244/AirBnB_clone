31f5620cc4e8d29a41e538891c55a57ab9cd6a09
Dcode3244 <dagimourgie111@gmail.com>
2022-11-27 08:36:21 -0900

    pycodestyle fix

--- console.py
+++ console.py
@@ -14,6 +14,7 @@ from models.place import Place
 from models.review import Review
 import models
 
+
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
     brackets = re.search(r"\[(.*?)\]", arg)
