c95ea3e322e6ee4ef63807647a92a07a159cd6d2
Dcode3244 <dagimourgie111@gmail.com>
2022-11-27 07:24:10 -0900

    modified console.py

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 
 import cmd
 import re
+from shlex import split
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models.user import User
@@ -12,6 +13,24 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 import models
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBcommand(cmd.Cmd):
@@ -78,7 +97,7 @@ class HBNBcommand(cmd.Cmd):
         """prints the string representation of an instance based
         on the class name and id"""
         objD = models.storage.all()
-        argList = args.split()
+        argList = parse(args)
         if len(argList) == 0:
             print("** class name missing **")
         elif argList[0] not in HBNBcommand.__classList:
@@ -95,7 +114,7 @@ class HBNBcommand(cmd.Cmd):
     def do_destroy(self, args):
         """deletes an instance based on the class name and id"""
         objD = models.storage.all()
-        argList = args.split()
+        argList = parse(args)
         if len(argList) == 0:
             print("** class name missing **")
         elif argList[0] not in HBNBcommand.__classList:
@@ -114,7 +133,7 @@ class HBNBcommand(cmd.Cmd):
         """ prints all string representation of all instances based or not
         on the class name"""
         objD = models.storage.all()
-        argList = arg.split()
+        argList = parse(arg)
         objList = []
         if len(argList) > 0 and argList[0] not in HBNBcommand.__classList:
             print("** class doesn't exist **")
@@ -135,7 +154,8 @@ class HBNBcommand(cmd.Cmd):
         Usage: update <class name> <id> <attribute name> "<attribute value>
         """
         objD = models.storage.all()
-        argL = arg.split()
+        argL = parse(arg)
+
         if len(argL) == 0:
             print("** class name missing **")
         elif argL[0] not in HBNBcommand.__classList:
@@ -147,16 +167,34 @@ class HBNBcommand(cmd.Cmd):
         elif len(argL) < 3:
             print("** attribute name missing **")
         elif len(argL) < 4:
-            print("** value missing **")
-        else:
-            for obj in objD.values():
-                obj.__dict__[argL[2]] = str(argL[3][1:len(argL[3]) - 1])
-                models.storage.save()
+            try:
+                type(eval(argL[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
+
+        if len(argL) == 4:
+            obj = objD["{}.{}".format(argL[0], argL[1])]
+            if argL[2] in obj.__class__.__dict__.keys():
+                valtype = type(obj.__class__.__dict__[argL[2]])
+                obj.__dict__[argL[2]] = valtype(argL[3])
+            else:
+                obj.__dict__[argL[2]] = argL[3]
+        elif type(eval(argL[2])) == dict:
+            obj = objD["{}.{}".format(argL[0], argL[1])]
+            for k, v in eval(argL[2]).items():
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
+                else:
+                    obj.__dict__[k] = v
+        models.storage.save()
 
     def do_count(self, arg):
         """Usage: count <class> or <class>.count()
         Retrieve the number of instances of a given class."""
-        argl = arg.split()
+        argl = parse(arg)
         count = 0
         for obj in models.storage.all().values():
             if argl[0] == obj.__class__.__name__:
